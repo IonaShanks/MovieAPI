541ac5a690814ebc89fb8f90f8bb2c8c8c5df0dd
Ona <Ona@ONA-PC>
2016-11-18 00:48:39 +0000

    Add project files.

---
+++ MovieAPI/Models/Cinema.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieAPI.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public String CinemaID { get; set; }
+        public String Name { get; set; }
+        [Url]
+        public String Website { get; set; }
+        public String PhoneNumber { get; set; }
+        public double TicketPrice { get; set; }
+        public double RunTime { get; set; }
+    }
+    
+}
---
+++ MovieAPI/Models/MovieListing.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieAPI.Models
+{
+    public enum Certification
+    {
+        [Display(Name = "G")]
+        G,
+        [Display(Name = "PG")]
+        PG,
+        [Display(Name = "12")]
+        Twelve,
+        [Display(Name = "12A")]
+        TwelveA,
+        [Display(Name = "15")]
+        Fifteen,
+        [Display(Name = "18")]
+        Eighteen
+    };
+    public class MovieListing
+    {
+        //Film ID title certification genre description running time
+        [Key, Required]
+        public String FilmID { get; set; }
+        [Required]
+        public String Title { get; set; }
+        [Required]
+        public Certification Certification { get; set; }
+        [Required]
+        public String Genre { get; set; }
+        public String Description { get; set; }
+        [Required]
+        // Minutes
+        public double RunTime { get; set; }
+
+    }
+
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("MovieListing")
+        {
+
+        }
+        public DbSet<MovieListing> Listings { get; set; }
+        //public DbSet<Cinema> Cinemas { get; set; }
+    }
+}
