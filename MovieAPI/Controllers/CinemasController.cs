904214af13b7323c20752deb817017924f078826
Ona <Ona@ONA-PC>
2016-11-22 20:43:53 +0000

    Added Scaffolded Cinemas Controller.

---
+++ MovieAPI/Controllers/CinemasController.cs
@@ -0,0 +1,129 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MovieModel;
+
+namespace MovieAPI.Controllers
+{
+    public class CinemasController : ApiController
+    {
+        private MovieContext db = new MovieContext();
+
+        // GET: api/Cinemas
+        public IQueryable<Cinema> GetCinemas()
+        {
+            return db.Cinemas;
+        }
+
+        // GET: api/Cinemas/5
+        [ResponseType(typeof(Cinema))]
+        public IHttpActionResult GetCinema(string id)
+        {
+            Cinema cinema = db.Cinemas.Find(id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cinema);
+        }
+
+        // PUT: api/Cinemas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCinema(string id, Cinema cinema)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cinema.CinemaID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cinema).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CinemaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cinemas
+        [ResponseType(typeof(Cinema))]
+        public IHttpActionResult PostCinema(Cinema cinema)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cinemas.Add(cinema);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CinemaExists(cinema.CinemaID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cinema.CinemaID }, cinema);
+        }
+
+        // DELETE: api/Cinemas/5
+        [ResponseType(typeof(Cinema))]
+        public IHttpActionResult DeleteCinema(string id)
+        {
+            Cinema cinema = db.Cinemas.Find(id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            db.Cinemas.Remove(cinema);
+            db.SaveChanges();
+
+            return Ok(cinema);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CinemaExists(string id)
+        {
+            return db.Cinemas.Count(e => e.CinemaID == id) > 0;
+        }
+    }
+}
